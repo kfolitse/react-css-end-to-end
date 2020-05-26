9b3343259265f22708afede6443b6a24bc4f99f3
Amenyo Folitse <foliamenyo@hotmail.com>
2020-05-26 12:39:57 -0400

    babel configuration

--- dist/bundle.esm.js
+++ dist/bundle.esm.js
@@ -1,5 +1,4 @@
 // import React from 'react'
 // const Spinners = props => <h1>Spinners</h1>
 // export default Spinners
-
 console.log('Hello' + 'allo' + 'my friends' + 'Salamatou')
---
+++ dist/bundle.umd.js
@@ -0,0 +1,10 @@
+;(function (factory) {
+  typeof define === 'function' && define.amd ? define(factory) : factory()
+})(function () {
+  'use strict'
+
+  // import React from 'react'
+  // const Spinners = props => <h1>Spinners</h1>
+  // export default Spinners
+  console.log('Hello' + 'allo' + 'my friends' + 'Salamatou')
+})
